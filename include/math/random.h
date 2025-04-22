11591d2513730f8c99f1756dae0446e824bed22a
Miguel Certã <miguelpc.sec@gmail.com>
2025-04-22 10:59:48 +0100

    Random functions

---
+++ include/math/random.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <time.h>
+#include <cstdlib>
+
+// # Returns a random number between 0 and 9.
+inline int random10Int() {
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    unsigned int seed = ts.tv_nsec;
+    unsigned int result = (1103515245 * seed + 12345) % 10;
+    unsigned int mask = result >> 31;
+    return (result + mask);
+}
+
+// # Weirdest random number in range generator.
+inline float randomRange(float min, float max) {
+    srand(random10Int() + 10 * random10Int() + 100 * random10Int() + 0.1 * random10Int());
+    return min + (rand() / (float) RAND_MAX) * (max-min);
+}
