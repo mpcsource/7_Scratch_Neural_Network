92ff5231c958f0eb268ba921a15494d3199c5869
Miguel Certã <miguelpc.sec@gmail.com>
2025-04-23 09:51:35 +0100

    Data normalization tests

--- tests/data_tests.cpp
+++ tests/data_tests.cpp
@@ -20,3 +20,14 @@ TEST(RandomTests, Range) {
     for(int i = 0; i < 10; i++)
         std::cout << randomRange(-1.0f, 2.0f) << std::endl;
 }
+
+TEST(Data, Normalization) {
+    Matrix<float> data = loadData<float>("../tests/data.csv", ',', true);
+
+    auto [x_train, y_train, x_test, y_test] = trainTestSplit<float>(data, 8);
+    x_train.head().basicPrint();
+    std::vector<float> a, b;
+    std::tie(x_train, a, b) = normalizeData<float>(x_train.head());
+    x_train.head().basicPrint();
+    Matrix<float>(1,8,b).basicPrint();
+}
